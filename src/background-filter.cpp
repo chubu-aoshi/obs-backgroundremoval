6c8a6556be2ba71ff566bfa4fa4fe461ddd15886
chubu-aoshi <157700613+chubu-aoshi@users.noreply.github.com>
2024-01-31 17:44:11 +0900

    Update background-filter.cpp

--- src/background-filter.cpp
+++ src/background-filter.cpp
@@ -718,8 +718,8 @@ void background_filter_video_render(void *data, gs_effect_t *_effect)
 		}
 	}
 
-	cv::Mat dst = tf->inputBGRA.clone();
-
+	cv::Mat dst;
+	cv::cvtColor(tf->inputBGRA, dst, cv::COLOR_BGRA2GRAY);
 	gs_texture_t *inputTexture = gs_texture_create(
 		dst.cols, dst.rows, GS_BGRA, 1, (const uint8_t **)&dst.data, 0);
 
